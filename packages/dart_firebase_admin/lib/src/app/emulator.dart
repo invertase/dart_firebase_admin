46749f3b6218faae506c990931ce8726de7e34d2
Willie Chang <willie.chang@positivegrid.com>
2024-06-10 16:06:39 +0800

    More doc for [Emulator] and make the constructor public.

--- packages/dart_firebase_admin/lib/src/app/emulator.dart
+++ packages/dart_firebase_admin/lib/src/app/emulator.dart
@@ -3,16 +3,23 @@ part of '../app.dart';
 /// Represents a Firebase Emulator.
 /// Now for Auth and Firestore.
 class Emulator {
-  const Emulator._(this.host, this.port);
 
+  /// If there is no environment variable set, please use this constructor.
+  /// For example, gcloud firestore emulators.
+  const Emulator(this.host, this.port);
+
+  /// The default Auth Emulator.
   const Emulator.defaultAuth()
       : host = '127.0.0.1',
         port = 9099;
 
+  /// The default Firestore Emulator.
   const Emulator.defaultFirestore()
       : host = '127.0.0.1',
         port = 8080;
 
+  /// Creates an [Emulator] from a firebase environment variable.
+  /// For example, env key for auth is: FIREBASE_AUTH_EMULATOR_HOST
   factory Emulator.fromEnvString(String envString) {
     final parts = envString.split(':');
     if (parts.length != 2) {
@@ -23,7 +30,7 @@ class Emulator {
     if (port == null) {
       throw ArgumentError.value(envString, 'envString', 'Invalid port');
     }
-    return Emulator._(host, port);
+    return Emulator(host, port);
   }
 
   final String host;
