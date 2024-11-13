d371bb2ce013a48dd32a4b64dcdadb008206ab53
evandro <evandro.bubiak@gmail.com>
2024-11-13 10:33:43 -0300

    created Firestore Errors Codes enum

--- packages/dart_firebase_admin/lib/src/google_cloud_firestore/firestore_exception.dart
+++ packages/dart_firebase_admin/lib/src/google_cloud_firestore/firestore_exception.dart
@@ -44,3 +44,88 @@ class FirebaseFirestoreAdminException extends FirebaseAdminException {
   String toString() =>
       'FirebaseFirestoreAdminException: $code: $message ${serverError.jsonResponse} ';
 }
+
+/// Auth client error codes and their default messages.
+enum FirestoreErrorCode {
+  aborted(
+    code: 'aborted',
+    message:
+        'The operation was aborted, typically due to a concurrency issue like transaction aborts, etc.',
+  ),
+  alreadyExists(
+    code: 'already-exists',
+    message: 'Some document that we attempted to create already exists.',
+  ),
+  cancelled(
+    code: 'cancelled',
+    message: 'The operation was cancelled (typically by the caller).',
+  ),
+  dataLoss(
+    code: 'data-loss',
+    message: 'Unrecoverable data loss or corruption.',
+  ),
+  deadlineExceeded(
+    code: 'deadline_exceeded',
+    message: 'Deadline expired before operation could complete.',
+  ),
+  failedPrecondition(
+    code: 'failed_precondition',
+    message:
+        "Operation was rejected because the system is not in a state required for the operation's execution.",
+  ),
+  internal(
+    code: 'internal',
+    message: 'Internal errors.',
+  ),
+  invalidArgument(
+    code: 'invalid_argument',
+    message: 'Client specified an invalid argument.',
+  ),
+  notFound(
+    code: 'not_found',
+    message: 'Some requested document was not found.',
+  ),
+  ok(
+    code: 'ok',
+    message: 'The operation completed successfully.',
+  ),
+  outOfRange(
+    code: 'out_of_range',
+    message: 'Operation was attempted past the valid range.',
+  ),
+  permissionDenied(
+    code: 'permission_denied',
+    message:
+        'The caller does not have permission to execute the specified operation.',
+  ),
+  resourceExhausted(
+    code: 'resource_exhausted',
+    message:
+        'Some resource has been exhausted, perhaps a per-user quota, or perhaps the entire file system is out of space.',
+  ),
+  unauthenticated(
+    code: 'unauthenticated',
+    message:
+        'The request does not have valid authentication credentials for the operation.',
+  ),
+  unavailable(
+    code: 'unavailable',
+    message: 'The service is currently unavailable.',
+  ),
+  unimplemented(
+    code: 'unimplemented',
+    message: 'Operation is not implemented or not supported/enabled.',
+  ),
+  unknown(
+    code: 'unknown',
+    message: 'Unknown error or an error from a different error domain.',
+  );
+
+  const FirestoreErrorCode({
+    required this.code,
+    required this.message,
+  });
+
+  final String code;
+  final String message;
+}
