c3e470f28d1ad9402480e2b6b70ddea000f1ba41
Elliot Hesp <elliot.hesp@gmail.com>
2025-11-17 10:21:08 +0000

    chore: Add base package

---
+++ packages/googleapis_dart_storage/example/googleapis_dart_storage_example.dart
@@ -0,0 +1,6 @@
+import 'package:googleapis_dart_storage/googleapis_dart_storage.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/googleapis_dart_storage/test/googleapis_dart_storage_test.dart
@@ -0,0 +1,16 @@
+import 'package:googleapis_dart_storage/googleapis_dart_storage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
