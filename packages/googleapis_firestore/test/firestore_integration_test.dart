094d5f0d0c678838e3cb9e0d4c4293ac06d3be60
demolaf <demolafadumo@gmail.com>
2026-02-25 11:25:40 +0100

    test: add tests for Firestore map keys containing '/' characters

--- packages/googleapis_firestore/test/firestore_integration_test.dart
+++ packages/googleapis_firestore/test/firestore_integration_test.dart
@@ -20,5 +20,36 @@ void main() {
 
       expect(collections, containsAll([a, b]));
     });
+
+    group('map keys with "/" characters', () {
+      test('set() round-trips a map with "/" in key', () async {
+        final docRef = firestore.doc('activities/new-activity');
+
+        await docRef.set({
+          'activityType': 'activityA',
+          'agents': {'products/product-a': 5.0},
+        });
+
+        final data = (await docRef.get()).data()!;
+        expect(data['activityType'], 'activityA');
+        expect(
+          (data['agents']! as Map<String, Object?>)['products/product-a'],
+          5.0,
+        );
+      });
+
+      test('update() round-trips a map value with "/" in key', () async {
+        final docRef = firestore.doc('activities/update-activity');
+        await docRef.set({'activityType': 'activityA'});
+
+        await docRef.update({'agents': {'products/product-b': 10.0}});
+
+        final data = (await docRef.get()).data()!;
+        expect(
+          (data['agents']! as Map<String, Object?>)['products/product-b'],
+          10.0,
+        );
+      });
+    });
   });
 }
